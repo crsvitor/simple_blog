9a030870759ec55f4d395d0440c1ce460cbd445a
Vitor de Castro <dev.castrors@gmail.com>
2022-07-29 00:01:37 -0300

    feat: timeline compoent

---
+++ src/components/Timeline.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const periods = ["Today", "This Week", "This Month"] as const;
+
+type Period = typeof periods[number]; 
+
+const selectedPeriod = ref<Period>("Today");
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
