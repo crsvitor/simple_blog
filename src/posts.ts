fafefbd952968a14670d12e598559ad09d5b1d24
Vitor de Castro <dev.castrors@gmail.com>
2022-08-01 21:45:56 -0300

    refactor: change post interface file location

--- src/posts.ts
+++ src/posts.ts
@@ -6,19 +6,19 @@ export interface Post {
   created: string;
 }
 
-export const Today:Post = {
+export const today: Post = {
   id: "1",
   title: "Today",
   created: DateTime.now().toISO()
 }
 
-export const ThisWeek:Post = {
+export const thisWeek: Post = {
   id: "2",
   title: "This Week",
   created: DateTime.now().minus({ days: 5 }).toISO()
 }
 
-export const ThisMonth:Post = {
+export const thisMonth: Post = {
   id: "3",
   title: "This Month",
   created: DateTime.now().minus({ weeks: 3 }).toISO()
