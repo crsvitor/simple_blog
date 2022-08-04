6bbcf4370cf90fb960083a4555a9ce8bbe09d615
Vitor de Castro <dev.castrors@gmail.com>
2022-08-03 23:12:31 -0300

    refactor: using pinia instead of our own composable

--- src/store/posts.ts
+++ src/store/posts.ts
@@ -1,36 +1,18 @@
-import { reactive, readonly } from 'vue';
-/**
-  reactive -> ref for objects {}, Map, Set and not primitives data
-  readonly -> it will reactively update the dom but won't let modify the value
-*/
+import { defineStore } from "pinia";
 
 interface PostState {
   foo: string;
 }
 
-export class PostsStore {
-  #state: PostState;
 
-  constructor() {
-    this.#state = reactive<PostState>({
-      foo: 'foo'
-    })
-  }
+export const usePosts = defineStore("posts", {
+  state: (): PostState => ({
+    foo: "foo"
+  }),
 
-  getState() {
-    return readonly(this.#state);
-  }
-
-  updateFoo(foo: string) {
-    this.#state.foo = foo;
-  }
-}
-
-const store = new PostsStore();
-
-/**
-  composables -> a way to get data through different contexts and components
- */
-export function usePosts() {
-  return store;
-}
+  actions: {
+    updateFoo(foo: string) {
+      this.foo = foo
+    }
+  },
+});
