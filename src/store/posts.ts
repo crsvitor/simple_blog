06bc54f0a5c27b69ef8b2d2a8b0e36d0b5067078
Vitor de Castro <dev.castrors@gmail.com>
2022-08-09 20:25:40 -0300

    refactor: updating PostState type and code related

--- src/store/posts.ts
+++ src/store/posts.ts
@@ -1,18 +1,23 @@
 import { defineStore } from "pinia";
+import { Post, thisMonth, thisWeek, today } from '../posts';
 
 interface PostState {
-  foo: string;
+  ids: string[];
+  all: Map<string, Post>
 }
 
 
 export const usePosts = defineStore("posts", {
   state: (): PostState => ({
-    foo: "foo"
+    ids: [today.id, thisWeek.id, thisMonth.id],
+    all: new Map([
+      [today.id, today],
+      [thisWeek.id, thisWeek],
+      [thisMonth.id, thisMonth]
+    ])
   }),
 
   actions: {
-    updateFoo(foo: string) {
-      this.foo = foo
-    }
+    // ...
   },
 });
